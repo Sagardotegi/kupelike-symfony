0cb653803ebbc0f2f1404c1ce5e8aaf35c364d6f
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2016-11-28 11:13:46 +0000

    sagardotegi guztiak mapa batean egiten

--- src/KupelikeBundle/Controller/IndexController.php
+++ src/KupelikeBundle/Controller/IndexController.php
@@ -4,6 +4,8 @@ namespace KupelikeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use KupelikeBundle\Entity\Sagardotegi;
+use KupelikeBundle\Entity\Kupela;
 
 class IndexController extends Controller
 {
