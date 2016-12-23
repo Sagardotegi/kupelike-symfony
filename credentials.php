8d3ccdca42ba4bb5aa1694717ff220c09807f402
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2016-12-23 09:06:37 +0000

    likeak ikusten dira

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+class Credentials
+{
+    public static $appId = '765683296917544';
+    public static $appSecret = '31ad4ff7d32353f15149a55b4b965596';
+    public static $appAccessToken = '765683296917544|31ad4ff7d32353f15149a55b4b965596';
+}
--- getlikes.php
+++ getlikes.php
@@ -1,21 +1,36 @@
 <?php
 // https://developers.facebook.com/docs/graph-api/reference/v2.8/object/likes
 include 'credentials.php';
+session_start();
+//use Facebook\Facebook;
+//require_once 'src/Facebook/autoload.php';
 // include composer autoload
-require 'vendor/autoload.php';
-$fb = new \Facebook\Facebook([
+//require_once('../vendor/autoload.php');
+
+//require_once 'src/Facebook/autoload.php';
+//require_once __DIR__ . '/vendor/autoload.php';
+//require_once('vendor/autoload.php');
+
+
+
+require_once('vendor/Facebook/src/Facebook/autoload.php');
+$fb = new Facebook\Facebook([
   'app_id' => Credentials::$appId,
   'app_secret' => Credentials::$appSecret,
   'default_access_token' => Credentials::$appAccessToken, // optional  
   'default_graph_version' => 'v2.8'
 ]);
+//$helper = $fb->getRedirectLoginHelper();
+
+//S$fb->setDefaultAccessToken((string) $_SESSION['fb_access_token']);
 try {
   // {object_id} 
   //      391578134319876 : Zubiri-Manteo
   //      391578134319876_954015738076110: One Zubiri-Manteo's post
   //      You can get de post ID From the 'embed' option of the post
   
-  $response = $fb->get('/391578134319876_954015738076110/likes');
+  //$response = $fb->get('/391578134319876_954015738076110/likes');
+  $response = $fb->get('/1704315726496042_1710432339217714/likes');
   
 } catch(\Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
