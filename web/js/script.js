03d710b79b91101f6911a5e6ffccf25f1d165175
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2017-01-31 11:50:29 +0000

    idiomas añadido

--- web/js/script.js
+++ web/js/script.js
@@ -8,7 +8,11 @@ $(document).ready(function($) {
         $('#searchIcon').toggleClass('fa-times');
         $('.searchResults').slideToggle(1000);
     });
+    
+    cambiarActive();
     //$('#search').textinput('option','preventFocusZoom',false);
+    
+    
 });
 
 
@@ -53,14 +57,48 @@ function zoomEnable(){
     
 /* APP info script */
 
-$(document).ready(function($) {
+/*$(document).ready(function($) {
   
       $('.info-icon').hover(function(){
           $(this).slideToggle(1000);
           $(this).siblings('.info-textarea').slideToggle(1000);
        
     });
-});
+});*/
+
+function cambiarActive(){
+    var url = window.location.pathname;
+    
+    if ( url.indexOf("/es/") > -1 ) {
+        $('.es').removeClass('lang-non-active');
+        $('.es').addClass('lang-active');
+        $('.eus').removeClass('lang-active');
+        $('.en').removeClass('lang-active');
+        $('.eus').addClass('lang-non-active');
+        $('.en').addClass('lang-non-active');
+    } else if( url.indexOf("/eus/") > -1 ){
+        $('.eus').removeClass('lang-non-active');
+        $('.eus').addClass('lang-active');
+        $('.es').removeClass('lang-active');
+        $('.en').removeClass('lang-active');
+        $('.es').addClass('lang-non-active');
+        $('.en').addClass('lang-non-active');
+    } else if( url.indexOf("/en/") > -1 ){
+        $('.en').removeClass('lang-non-active');
+        $('.en').addClass('lang-active');
+        $('.eus').removeClass('lang-active');
+        $('.es').removeClass('lang-active');
+        $('.eus').addClass('lang-non-active');
+        $('.es').addClass('lang-non-active');
+    } else {
+        $('.es').removeClass('lang-active');
+        $('.eus').removeClass('lang-active');
+        $('.en').removeClass('lang-active');
+        $('.es').addClass('lang-non-active');
+        $('.eus').addClass('lang-non-active');
+        $('.en').addClass('lang-non-active');
+    }
+}
 
 
 /* APP info script */
